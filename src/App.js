3444a107cac516b666ac537d75945c8ba68f394e
Sanjay Gonsalves <snjgonsalves@gmail.com>
2022-02-17 09:52:11 -0800

    Added all files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Records from './Records.js';
+
+function App() {
+  return (
+<main className="container">
+      <Records />
+    </main>
+  );
+}
+
+export default App;
